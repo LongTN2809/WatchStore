d237299403b0e6193afa3bf1b29e93c85fc4e6fb
Long <elilinhh@gmail.com>
2025-10-23 22:47:51 +0700

    first commit

---
+++ JS/home.js
@@ -0,0 +1,10 @@
+<<<<<<< HEAD
+let buttonToGoProductPage = document.getElementById("goToProduct");
+buttonToGoProductPage.addEventListener("click",()=>{
+   window.location.href = "product.html";
+=======
+let buttonToGoProductPage = document.getElementById("goToProduct");
+buttonToGoProductPage.addEventListener("click",()=>{
+   window.location.href = "product.html";
+>>>>>>> ec6bb0edaca241c731b210e94383c9043780dfa9
+});
