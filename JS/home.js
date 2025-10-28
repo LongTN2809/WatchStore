dd5825eea0678d6eec8a464c976b5de9a4e1a7db
Long <elilinhh@gmail.com>
2025-10-28 22:34:27 +0700

    Change all

--- JS/home.js
+++ JS/home.js
@@ -1,10 +1,14 @@
-<<<<<<< HEAD
-let buttonToGoProductPage = document.getElementById("goToProduct");
-buttonToGoProductPage.addEventListener("click",()=>{
-   window.location.href = "product.html";
-=======
-let buttonToGoProductPage = document.getElementById("goToProduct");
-buttonToGoProductPage.addEventListener("click",()=>{
-   window.location.href = "product.html";
->>>>>>> ec6bb0edaca241c731b210e94383c9043780dfa9
+const boxes = document.querySelectorAll(".reveal");
+window.addEventListener('scroll' , ()=>{
+    // Dung 4/5 phan man hinh de lam viewport va tinh khoang cach voi phan tu khac
+    const triggerBottom = window.innerHeight / 5 * 4;
+    boxes.forEach(box=>{
+      // Lay vi tri that su cua phan tu
+      const boxTop = box.getBoundingClientRect().top;
+      if(triggerBottom > boxTop){
+         box.classList.add("show");
+      }else{
+         box.classList.remove("show");
+      }
+    });
 });
